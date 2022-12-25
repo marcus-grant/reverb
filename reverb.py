ce06a3c40ffc14335bea2d4ae07546e831c66b43
Marcus Grant <marcusfg@gmail.com>
2022-12-25 13:28:37 +0100

    Fix argparsing for zone,auth,sub

--- reverb.py
+++ reverb.py
@@ -10,24 +10,48 @@ from urllib.request import urlopen, Request
 @dataclass(init=False)
 class ReverbOptions:
     PROG_DESC = 'HTTP echo/mirror utilities scripts'
-    ENV_VARS = {'REVERB_SERVER', 'REVERB_DDNS_AUTH', 'REVERB_DDNS_ZONEID',
-        'REVERB_DDNS_EMAIL', 'REVERB_DDNS_DOMAIN', 'REVERB_DDNS_SUBDOMAIN'}
+    ENV_REVERB_SERVER = 'REVERB_SERVER'
+    ENV_REVERB_DDNS_AUTH = 'REVERB_DDNS_AUTH'
+    ENV_REVERB_DDNS_EMAIL = 'REVERB_DDNS_EMAIL'
+    ENV_REVERB_DDNS_DOMAIN = 'REVERB_DDNS_DOMAIN'
+    ENV_REVERB_DDNS_ZONEID = 'REVERB_DDNS_ZONEID'
+    ENV_REVERB_DDNS_SUBDOMAIN = 'REVERB_DDNS_SUBDOMAIN'
+    ENV_VARS = {ENV_REVERB_SERVER, ENV_REVERB_DDNS_AUTH, ENV_REVERB_DDNS_ZONEID,
+        ENV_REVERB_DDNS_EMAIL, ENV_REVERB_DDNS_DOMAIN, ENV_REVERB_DDNS_SUBDOMAIN}
+    
+    CMD_IP = 'ip'
+    CMD_DDNS = 'ddns'
+    CMD_DDNS_LIST = 'ddns-list'
+    CMD_DDNS_SET = 'ddns-set'
+    ROOT_CMDS = [CMD_IP, CMD_DDNS]
+    DDNS_CMDS = [CMD_DDNS_LIST, CMD_DDNS_SET]
+    CMDS = [CMD_IP, CMD_DDNS, CMD_DDNS_LIST, CMD_DDNS_SET]
+
+    ARG_SERVER, ARG_DDNS_PROVIDER = ('server', 'ddns-provider')
+    ARG_DDNS_DOMAIN, ARG_DDNS_ZONE = ('ddns-domain', 'ddns-zone')
+    ARG_DDNS_AUTH, ARG_DDNS_EMAIL = ('ddns-auth', 'ddns-email')
+    ARG_SUBDOMAIN = 'ddns-subdomain'
+
+    ALL_ARGS_LONG = ['server', 'ddns-provider', 'ddns-domain', 'ddns-zone',
+        'ddns-auth', 'ddns-email', 'ddns-subdomain']
+    ALL_ARGS_SHRT = ['s', 'p', 'd', 'z', 'a', 'e', 'n']
 
     USAGES = {
-        'ip': 'Send request to reverb server to discover client\'s public IP address',
-        'ddns': 'Perform commands with respect to a (D)DNS provider',
-        'ddns-set': "Get public IP & modify a DNS providers' A record with that IP",
-        'ddns-list': ("List a DDNS provider's domains or "
+        CMD_IP: 'Send request to reverb server to discover client\'s public IP address',
+        CMD_DDNS: 'Perform commands with respect to a (D)DNS provider',
+        CMD_DDNS_SET: "Get public IP & modify a DNS providers' A record with that IP",
+        CMD_DDNS_LIST: ("List a DDNS provider's domains or "
             "records for a domain if a --domain arg given"),
-        'provider': ('DDNS provider to use ' +
+        ARG_DDNS_PROVIDER: ('DDNS provider to use ' +
             '[cloudflare|bunny|duck](default: cloudflare)'),
-        'ddns_domain': 'Domain name to apply dynamic DNS address to (default: blank)',
-        'ddns_zoneid': ('ZoneID for DDNS providers that' +
+        ARG_DDNS_DOMAIN: 'Domain name to apply dynamic DNS address to (default: blank)',
+        ARG_DDNS_ZONE: ('ZoneID for DDNS providers that' +
             'provide IDs to their domains/zones'),
-        'server': 'Reverb server URL/address to use (required)',
-        'auth': "An API authentication token to use for commnads requiring it",
-        'email': "A user email address to use with authentication if required",
-        'subdomain': "A subdomain or name to apply DDNS updates to (default: '@')"
+        ARG_SERVER: 'Reverb server URL/address to use (required)',
+        ARG_DDNS_AUTH: "An API authentication token to use for commnads requiring it",
+        ARG_DDNS_EMAIL: "A user email address to use with authentication if required",
+        ARG_SUBDOMAIN: ("A subdomain or name to apply " +
+            "DDNS updates to (default: '@')")
     }
 
     def __init__(self, envars=None, argv=None):
@@ -58,18 +82,32 @@ class ReverbOptions:
             else:
                 enval = envars[key]
             if enval:
-                match key:
-                    case 'REVERB_SERVER': self.reverb_server = enval
-                    case 'REVERB_DDNS_AUTH': self.ddns_auth = enval
-                    case 'REVERB_DDNS_EMAIL': self.ddns_email = enval
-                    case 'REVERB_DDNS_DOMAIN': self.ddns_domain = enval
-                    case 'REVERB_DDNS_ZONEID': self.ddns_zoneid = enval
-                    case 'REVERB_DDNS_SUBDOMAIN': self.ddns_subdomain = enval
-                    case _:
-                        print(f"ERROR: The given environ var of key {key} is invalid")
-                        raise KeyError
+                if key == self.ENV_REVERB_SERVER: self.reverb_server = enval
+                elif key == self.ENV_REVERB_DDNS_AUTH: self.ddns_auth = enval
+                elif key == self.ENV_REVERB_DDNS_ZONEID: self.ddns_zoneid = enval
+                elif key == self.ENV_REVERB_DDNS_EMAIL: self.ddns_email = enval
+                elif key == self.ENV_REVERB_DDNS_DOMAIN: self.ddns_email = enval
+                elif key == self.ENV_REVERB_DDNS_SUBDOMAIN: self.ddns_subdomain = enval
+                else:
+                    print(f"ERROR: The given environ var of key {key} is invalid")
+                    raise KeyError
+
+                # DELETEME : Decide if we should use the match syntax
+                # It was included in python 3.10 or late 2021
+                # Debian cant use it by default, almost certainly not EL8 either
+                # match key:
+                #     case 'REVERB_SERVER': self.reverb_server = enval
+                #     case 'REVERB_DDNS_AUTH': self.ddns_auth = enval
+                #     case 'REVERB_DDNS_EMAIL': self.ddns_email = enval
+                #     case 'REVERB_DDNS_DOMAIN': self.ddns_domain = enval
+                #     case 'REVERB_DDNS_ZONEID': self.ddns_zoneid = enval
+                #     case 'REVERB_DDNS_SUBDOMAIN': self.ddns_subdomain = enval
+                #     case _:
+                #         print(f"ERROR: The given environ var of key {key} is invalid")
+                #         raise KeyError
     
     def parse_args(self, argv=None):
+        # TODO: Create datastructures for each subparser and iterate instead
         self.argparser = ArgumentParser(
             prog='reverb', description=self.PROG_DESC)
 
@@ -97,33 +135,33 @@ class ReverbOptions:
         parser_ddns_list.add_argument('--server', '-s', 
             help=self.USAGES['server'])
         parser_ddns_list.add_argument(
-            '--provider', '-p', help=self.USAGES['provider'])
+            '--provider', '-p', help=self.USAGES[self.ARG_DDNS_PROVIDER])
         parser_ddns_list.add_argument(
-            '--ddns-domain', '-d', type=str, help=self.USAGES['ddns_domain'])
-        parser_ddns_list.add_argument(
-            '--ddns-zone', '-z', type=str, help=self.USAGES['ddns_zoneid'])
-        parser_ddns_list.add_argument(
-            '--auth', '-a', type=str, help=self.USAGES['auth'])
-        parser_ddns_list.add_argument(
-            '--email', '-e', type=str, help=self.USAGES['email'])
+            '--ddns-domain', '-d', type=str, help=self.USAGES[self.ARG_DDNS_DOMAIN])
+        parser_ddns_list.add_argument(f"--{self.ARG_DDNS_ZONE}", '-z',
+            type=str, help=self.USAGES[self.ARG_DDNS_ZONE])
+        parser_ddns_list.add_argument(f"--{self.ARG_DDNS_AUTH}", '-a',
+            type=str, help=self.USAGES[self.ARG_DDNS_AUTH])
+        parser_ddns_list.add_argument(f"--{self.ARG_DDNS_EMAIL}", '-e',
+            type=str, help=self.USAGES[self.ARG_DDNS_EMAIL])
         # 'set' sub-subcmd added to subcommand 'ddns'
         parser_ddns_set = subparser_ddns.add_parser(
             'set', help=self.USAGES['ddns-set'])
         # TODO investigate if --server can be defined near root subcommand parser
         parser_ddns_set.add_argument('--server', '-s', 
             help=self.USAGES['server'])
-        parser_ddns_set.add_argument(
-            '--provider', '-p', help=self.USAGES['provider'])
-        parser_ddns_set.add_argument(
-            '--ddns-zone', '-z', type=str, help=self.USAGES['ddns_zoneid'])
-        parser_ddns_set.add_argument(
-            '--ddns-domain', '-d', type=str, help=self.USAGES['ddns_domain'])
-        parser_ddns_set.add_argument(
-            '--ddns-subdomain', '-n', type=str, help=self.USAGES['subdomain'])
-        parser_ddns_set.add_argument(
-            '--auth', '-a', type=str, help=self.USAGES['auth'])
-        parser_ddns_set.add_argument(
-            '--email', '-e', type=str, help=self.USAGES['email'])
+        parser_ddns_set.add_argument('--ddns-provider', '-p',
+            help=self.USAGES[self.ARG_DDNS_PROVIDER])
+        parser_ddns_set.add_argument(f"--{self.ARG_DDNS_ZONE}", '-z',
+            type=str, help=self.USAGES[self.ARG_DDNS_ZONE])
+        parser_ddns_set.add_argument('--ddns-domain', '-d',
+            type=str, help=self.USAGES[self.ARG_DDNS_DOMAIN])
+        parser_ddns_set.add_argument('--ddns-subdomain', '-n',
+            type=str, help=self.USAGES[self.ARG_SUBDOMAIN])
+        parser_ddns_set.add_argument(f"--{self.ARG_DDNS_AUTH}", '-a',
+            type=str, help=self.USAGES[self.ARG_DDNS_AUTH])
+        parser_ddns_set.add_argument(f"--{self.ARG_DDNS_EMAIL}", '-e',
+            type=str, help=self.USAGES[self.ARG_DDNS_EMAIL])
         
         args = None
         if argv is None:
@@ -136,19 +174,33 @@ class ReverbOptions:
             val = getattr(args, key)
             if val is None:
                 continue
-            match key:
-                case 'root_subcmd': self.root_subcmd = val
-                case 'server': self.reverb_server = val
-                case 'ddns_subcmd': self.ddns_subcmd = val
-                case 'provider': self.ddns_provider = val
-                case 'ddns-zone': self.ddns_zoneid = val
-                case 'ddns-domain': self.ddns_domain = val
-                case 'ddns-subdomain': self.ddns_subdomain = val
-                case 'auth': self.ddns_auth = val
-                case 'email': self.ddns_email = val
-                case other:
-                    print(f"ERROR: The given argparsed argument of key {key} is invalid")
-                    raise KeyError
+            if key == 'root_subcmd': self.root_subcmd = val
+            elif key == 'ddns_subcmd': self.ddns_subcmd = val
+            elif key == self.ARG_SERVER: self.reverb_server = val
+            elif key == self.ARG_DDNS_PROVIDER: self.ddns_provider = val
+            elif key == 'ddns_zone': self.ddns_zoneid = val
+            elif key == 'ddns_domain': self.ddns_domain = val
+            elif key == 'ddns_subdomain': self.ddns_subdomain = val
+            elif key == 'ddns_auth': self.ddns_auth = val
+            elif key == 'ddns_email': self.ddns_email = val
+            else:
+                print(f"ERROR: The given argparsed argument of key {key} is invalid")
+                raise KeyError
+            # DELETEME TODO decide if we stop using match case
+            # since old systems dont use it
+            # match key:
+            #     case 'root_subcmd': self.root_subcmd = val
+            #     case 'server': self.reverb_server = val
+            #     case 'ddns_subcmd': self.ddns_subcmd = val
+            #     case 'provider': self.ddns_provider = val
+            #     case 'ddns-zone': self.ddns_zoneid = val
+            #     case 'ddns-domain': self.ddns_domain = val
+            #     case 'ddns-subdomain': self.ddns_subdomain = val
+            #     case 'auth': self.ddns_auth = val
+            #     case 'email': self.ddns_email = val
+            #     case other:
+            #         print(f"ERROR: The given argparsed argument of key {key} is invalid")
+            #         raise KeyError
 
 @dataclass
 class RequestHandler:
